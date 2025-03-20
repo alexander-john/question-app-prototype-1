7a89e7a2631990751b995412a115836ca3b338e8
Alexander John <96aljohn@gmail.com>
2025-03-19 18:36:14 -0700

    refactor: reorganize component structure and move pages to a dedicated directory

--- question-app/src/App.jsx
+++ question-app/src/App.jsx
@@ -1,10 +1,10 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Register from "./Register";
-import Login from "./Login";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 import QuestionPage from "./pages/QuestionPage"; // Updated path
-import MultipleChoicePage from "./MultipleChoicePage";
+import MultipleChoicePage from "./pages/MultipleChoicePage";
 import "./App.css";
 
 const App = () => {
